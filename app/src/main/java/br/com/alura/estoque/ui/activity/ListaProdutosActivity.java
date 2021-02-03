beb2af4318404975416a3e37c560a221fa4ffa70
DESKTOP-20POM8C\Pablo <pablo-pinheiro@hotmail.com>
2021-02-03 09:39:39 -0300

    migrando as chamadas assincronas para um repository

--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -16,6 +16,7 @@ import br.com.alura.estoque.asynctask.BaseAsyncTask;
 import br.com.alura.estoque.database.EstoqueDatabase;
 import br.com.alura.estoque.database.dao.ProdutoDAO;
 import br.com.alura.estoque.model.Produto;
+import br.com.alura.estoque.repository.ProdutoRepository;
 import br.com.alura.estoque.retrofit.EstoqueRetrofit;
 import br.com.alura.estoque.retrofit.service.ProdutoService;
 import br.com.alura.estoque.ui.dialog.EditaProdutoDialog;
@@ -42,36 +43,11 @@ public class ListaProdutosActivity extends AppCompatActivity {
         EstoqueDatabase db = EstoqueDatabase.getInstance(this);
         dao = db.getProdutoDAO();
 
-        buscaProdutos();
+        ProdutoRepository repository = new ProdutoRepository(dao);
+        repository.buscaProdutos(produtos -> { adapter.atualiza(produtos);});
     }
 
-    private void buscaProdutos() {
-        buscaProdutosInternos();
-    }
 
-    private void buscaProdutosInternos() {
-        new BaseAsyncTask<>( dao::buscaTodos, resultado -> {
-            adapter.atualiza(resultado);
-            buscaProdutosNaAPI();
-        }).execute();
-    }
-
-    private void buscaProdutosNaAPI() {
-        ProdutoService service = new EstoqueRetrofit().getProdutoService();
-        Call<List<Produto>> call = service.buscaTodos();
-        new BaseAsyncTask<>( () ->{
-            try{
-                Response<List<Produto>> resposta = call.execute();
-                List<Produto> produtosNovos = resposta.body();
-                dao.salva(produtosNovos);
-            }catch(IOException e){
-                e.printStackTrace();
-            } 
-            return dao.buscaTodos();
-        }, produtosNovos -> {
-                adapter.atualiza(produtosNovos);
-        }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-    }
 
     private void configuraListaProdutos() {
         RecyclerView listaProdutos = findViewById(R.id.activity_lista_produtos_lista);
